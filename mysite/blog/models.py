a3b894fd6509e62afc0b0fb89b930774ea638b25
jamoeba <jamoeba@live.com>
2018-09-03 08:48:56 +0800

    add targit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -31,6 +32,7 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
@@ -41,3 +43,18 @@ class Post(models.Model):
 
     def get_back(self):
         return reverse('blog:post_list')
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
