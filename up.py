f672b0141dcc650d7878fc49065020e7501ee343
Jamoeba <jamoeba@users.noreply.github.com>
2021-08-23 21:10:22 +0800

    Update up.py

--- up.py
+++ up.py
@@ -7,29 +7,26 @@ username = 'zjjm'
 password = 'ftp_123_zjjm'
 
 
-# 连接并登录
 def ftpconnect(host, port, username, password):
     ftp = FTP()
-    # ftp.set_debuglevel(2)         #打开调试级别2，显示详细信息
-    ftp.connect(host, port)  # 连接
-    ftp.login(username, password)  # 登录，如果匿名登录则用空串代替即可
+    # ftp.set_debuglevel(2)
+    ftp.connect(host, port)
+    ftp.login(username, password)
     return ftp
 
 
-# 下载文件
-def downloadfile(ftp, remotepath, localpath):  # remotepath：上传服务器路径；localpath：本地路径；
-    bufsize = 1024  # 设置缓冲块大小
-    fp = open(localpath, 'wb')  # 以写模式在本地打开文件
-    ftp.retrbinary('RETR ' + remotepath, fp.write, bufsize)  # 接收服务器上文件并写入本地文件
-    ftp.set_debuglevel(0)  # 关闭调试
-    fp.close()  # 关闭文件
+def downloadfile(ftp, remotepath, localpath):
+    bufsize = 1024
+    fp = open(localpath, 'wb')
+    ftp.retrbinary('RETR ' + remotepath, fp.write, bufsize)
+    ftp.set_debuglevel(0)
+    fp.close()
 
 
-# 上传文件
 def uploadfile(ftp, remotepath, localpath):
     bufsize = 1024
     fp = open(localpath, 'rb')
-    ftp.storbinary('STOR ' + remotepath, fp, bufsize)  # 上传文件
+    ftp.storbinary('STOR ' + remotepath, fp, bufsize)
     ftp.set_debuglevel(0)
     fp.close()
 
